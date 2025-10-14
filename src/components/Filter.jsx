4af6ecfa36fbc58ff5b941554f7783ced92b5ba0
Jamie Bryson <jamieab98@gmail.com>
2025-10-14 16:05:16 -0400

    created the onChange portion in the Filter.jsx file

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { useState } from "react";
 
 function Filter({ onCategoryChange }) {
+
+  const [searchItem, setSearchItem] = useState('');
+  const handleChange = (e) => {
+    setSearchItem(e.target.value);
+    console.log(searchItem);
+  }
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input type="text" name="search" placeholder="Search..." 
+        value={searchItem}
+        onChange={handleChange}
+      />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
